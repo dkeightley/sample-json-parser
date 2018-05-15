515add9efdb59e5b8b3ce8c1b8470bf7215a20a5
Derek <derekjkeightley@gmail.com>
2018-05-15 22:48:05 +1000

    Updates to index.html, added cats

--- parser.js
+++ parser.js
@@ -1,12 +1,13 @@
-$(document).ready(function () {
-  var jsonURL = "sample.json";
-  $.getJSON(jsonURL, function (json)
-  {
-    $.each(json.results, function (key, val) {
-      html = '<div class="image-list">';
-      html += '<img src ="' + val.image_url + '" class="image-styles" />';
-      html += '<p class="image-title">' + val.name + '</p>';
-      html += '</div>';
-      target.append(html);
-  });
+$(function(){
+    var jsonURL = "sample.json";
+    $.getJSON(jsonURL, function (data)
+    {
+      $.each(data.results, function (key, val) {
+        html = '<div class="image-list">';
+        html += '<img src ="' + val.img_url + '" class="image-styles" />';
+        html += '<p class="image-title">' + val.name + '</p>';
+        html += '</div>';
+        $('#target').append(html);
+      });
+    });
 });
